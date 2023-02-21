d679059cdcba0894534c48005c08a486673b097c
h587630 <587630@stud.hvl.no>
2023-02-21 12:27:19 +0100

    Messages er ferdig.

--- src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
+++ src/main/java/no/hvl/dat110/messages/DeleteTopicMsg.java
@@ -3,30 +3,26 @@ package no.hvl.dat110.messages;
 public class DeleteTopicMsg extends Message {
 
 	// message sent from client to create topic on the broker
-
+	// Object variable
 	private String topic;
 	
+	// Constructor
 	public DeleteTopicMsg(String user, String topic) {
 		super(MessageType.DELETETOPIC, user);
 		this.topic=topic;
 	}
-
+	// get-method
 	public String getTopic() {
 		return topic;
 	}
-
+	// set-method
 	public void setTopic(String topic) {
 		this.topic = topic;
 	}
-
+	// toString method
 	@Override
 	public String toString() {
 		return "CreateTopicMsg [topic=" + topic + super.toString() + "]";
 	}
-	// TODO:
-	// Implement object variables - a topic is required
-
-	// Complete the constructor, get/set-methods, and toString method
-	// as described in the project text
-
+	
 }
